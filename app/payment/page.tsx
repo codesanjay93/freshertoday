1ced97aab9c3349aab02341acdabcbaef05c8b6f
codesanjay <codesanjay93@gmail.com>
2025-07-11 23:36:28 +0530

    Deployment configuration

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,12 +1,12 @@
 // pages/pay.tsx
 
-import Razorpay from "../components/payments/razorpay/Razorpay";
+// import Razorpay from "../components/payments/razorpay/Razorpay";
 
 export default function PayPage() {
   return (
     <div className="p-6 h-screen w-screen flex items-center justify-center bg-[#f9f9f9] dark:bg-[#121212] flex-col">
       <h1 className="text-xl font-semibold mb-4">Pay ₹1 to Get Your Certificate</h1>
-      <Razorpay />
+      {/* <Razorpay /> */}
     </div>
   );
 }
